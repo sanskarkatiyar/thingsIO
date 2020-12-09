62e362a982fcff23448766fcf397836ec6dc44ee
casecarsid <siddhantkeshkar@gmail.com>
2020-12-09 08:29:53 +0000

    gke docker changes

--- deploy-local-dev.sh
+++ deploy-local-dev.sh
@@ -4,10 +4,25 @@
 # to identify the processes ids and then "kill -9 <pid>"
 kubectl apply -f redis/redis-deployment.yaml
 kubectl apply -f redis/redis-service.yaml
+
 kubectl apply -f rabbitmq/rabbitmq-deployment.yaml
 kubectl apply -f rabbitmq/rabbitmq-service.yaml
+
 kubectl apply -f influxdb/influxdb-deployment.yaml
 kubectl apply -f influxdb/influxdb-service.yaml
-kubectl port-forward --address 0.0.0.0 service/rabbitmq 5672:5672 &
-kubectl port-forward --address 0.0.0.0 service/redis 6379:6379 &
-kubectl port-forward --address 0.0.0.0 service/influxdb 8086:8086
+
+kubectl apply -f rest/rest-deployment.yaml
+kubectl apply -f rest/rest-service.yaml
+# kubectl apply -f rest/rest-ingress.yaml
+
+kubectl apply -f dashboard/dashboard-deployment.yaml
+kubectl apply -f dashboard/dashboard-service.yaml
+kubectl apply -f dashboard/dashboard-ingress.yaml
+
+kubectl apply -f analytics/analytics-deployment.yaml
+
+kubectl apply -f ingestor/ingestor-deployment.yaml
+
+# kubectl port-forward --address 0.0.0.0 service/rabbitmq 5672:5672 &
+# kubectl port-forward --address 0.0.0.0 service/redis 6379:6379 &
+# kubectl port-forward --address 0.0.0.0 service/influxdb 8086:8086
